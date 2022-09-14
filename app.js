2af56b03474cda1a2231e66b27db7919f6f7ee61
Ansible on Vagrant <ansible.on.vagrant@robnugen.com>
2022-09-14 01:36:03 +0000

    Hello, app middleware!
    https://codeforgeek.com/node-js-tutorial-step-by-step/

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ router.get("/home", (req, res) => {
     res.send("Hello from " + req.url.replace(/^\//, ''));  // drop leading slash from URL
 });
 
+app.use((req, res, next) => {
+    console.log('App middleware says, "stop, hammertime:', Date.now(), '"');
+  next();
+});
+
 app.use('/', router);
 
 app.listen(process.env.port || `${port}`, hostname, () => {
