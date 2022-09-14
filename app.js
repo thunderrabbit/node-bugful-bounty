3f28dccf4b2cd61dc3be5154ba4dd8a097ee662c
Ansible on Vagrant <ansible.on.vagrant@robnugen.com>
2022-09-14 01:23:45 +0000

    Add a GET route to express server

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const express = require('express');
-const server = express();
-
 const hostname = '0.0.0.0';
 const port = 4000;
+
+const express = require('express');
+const server = express();
+const router = express.Router();
+
+router.get("/home", (req, res) => {
+    res.send("Hello from " + req.url.replace(/^\//, ''));  // drop leading slash from URL
+});
+
+server.use('/', router);
 
 server.listen(process.env.port || `${port}`, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
