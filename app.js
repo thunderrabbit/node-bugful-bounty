f8a91400b62774e69e54a1aef2a1c8078e78460e
Ansible on Vagrant <ansible.on.vagrant@robnugen.com>
2022-09-14 01:50:01 +0000

    Hello, router middleware

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
+router.use((req, res, next) => {
+    console.log('Router middleware ', Date.now(), '"');
+  next();
+});
+
 router.get("/home", (req, res) => {
     res.send("Hello from " + req.url.replace(/^\//, ''));  // drop leading slash from URL
 });
 
+// add middleware before routes
 app.use((req, res, next) => {
     console.log('App middleware says, "stop, hammertime:', Date.now(), '"');
   next();
