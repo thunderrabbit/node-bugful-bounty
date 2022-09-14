1b8a913d16d922d6da668f25235f3aa264840521
Ansible on Vagrant <ansible.on.vagrant@robnugen.com>
2022-09-14 01:16:45 +0000

    Hello, express
    https://codeforgeek.com/node-js-tutorial-step-by-step/

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
-const http = require('express');
+const express = require('express');
+const server = express();
 
 const hostname = '0.0.0.0';
 const port = 4000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    switch(req.url) {
-    case "/home":
-	res.end('<h1>Hello Home</h1>');
-	break;
-    case "/about":
-	res.end('<h1>About last night</h1>');
-	break;
-    default:
-	res.end('<h1>Hello ' + req.url.replace(/^\//, '') + '</h1>');  // drop forward slash from URL
-	break;
-    }	
-    console.log("Serving " + req.url);
-});
-
-server.listen(port, hostname, () => {
+server.listen(process.env.port || `${port}`, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
